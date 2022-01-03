41b28d5feeab567bad87fdfae60c400a069f0c75
Aviril <89867985+Aviril@users.noreply.github.com>
2022-01-03 21:53:55 +0300

    Update compiler.cpp

--- Sandbox/Lua/compiler.cpp
+++ Sandbox/Lua/compiler.cpp
@@ -7,9 +7,28 @@
 
 std::mutex locker;
 std::string modulePath = "Lua/";
-static const wi::vector<std::string> SandboxModules = {
-   "Client/init.lua"
+struct moduleTypes
+{
+	std::string moduleName;
+	wi::vector<std::string> moduleScripts;
 };
+wi::vector<moduleTypes> SandboxModule = {
+    {
+        "Shared",
+        {
+            "init.lua"
+        }
+    },
+    {
+        "Client",
+        {}
+    },
+    {
+        "Server",
+        {}
+    }
+};
+
 wi::unordered_map<std::string, std::string> moduleResults;
 
 int main()
@@ -21,46 +40,53 @@ int main()
 	wi::helper::MakePathAbsolute(modulePath);
 	wi::Timer timer;
 
-	for (int i = 0; i < SandboxModules.size(); ++i)
+	for (int i = 0; i < SandboxModule.size(); ++i)
     {
-        wi::vector<uint8_t> fileData;
-        if (wi::helper::FileRead(modulePath + SandboxModules[i], fileData))
+        std::cout << SandboxModule[i].moduleName << std::endl;
+		for (int j = 0; j < SandboxModule[i].moduleScripts.size(); ++j)
 		{
-            std::string scriptData = std::string(fileData.begin(), fileData.end());
-            wi::jobsystem::Execute(ctx, [=](wi::jobsystem::JobArgs args) {
-				locker.lock();
-				std::cout << "Script Compiled: " << SandboxModules[i] << std::endl;
-				moduleResults[(SandboxModules[i])] = scriptData;
-				locker.unlock();
-			});
-		}
-    	else
-		{
-			locker.lock();
-			std::cerr << "Script Compilation Failed: " << SandboxModules[i] << std::endl;
-			locker.unlock();
-			std::exit(1);
+            wi::vector<uint8_t> fileData;
+            std::string scriptPath = SandboxModule[i].moduleScripts[j]
+            if (wi::helper::FileRead(modulePath + ScriptPath, fileData))
+            {
+                std::string scriptData = std::string(fileData.begin(), fileData.end());
+                wi::jobsystem::Execute(ctx, [=](wi::jobsystem::JobArgs args) {
+                    locker.lock();
+                    std::cout << "Script Compiled: " << ScriptPath << std::endl;
+                    //moduleResults[(ScriptPath)] = scriptData;
+                    locker.unlock();
+                });
+            }
+            else
+            {
+                locker.lock();
+                std::cerr << "Script Compilation Failed: " << ScriptPath << std::endl;
+                locker.unlock();
+                std::exit(1);
+            }
 		}
     }
 	wi::jobsystem::Wait(ctx);
 	std::cout << "[Sandbox Compiler] Finished compiling Lua module in " << std::setprecision(4) << timer.elapsed_seconds() << " seconds" << std::endl;
 	timer.record();
 
+    /*
     std::string bundlerData;
     bundlerData += "namespace SandboxLua {\n";
 	bundlerData += "wi::unordered_map<std::string, std::string> modules {\n";
+
+    bundlerData += "wi::unordered_map<std::string, std::string> " + "Client" + "[] = {}";
 	for (auto& x : moduleResults)
 	{
 		auto& name = x.first;
 		auto& output = x.second;
 
 		std::string name_repl = name;
-		std::replace(name_repl.begin(), name_repl.end(), '/', '_');
-		std::replace(name_repl.begin(), name_repl.end(), '.', '_');
-		bundlerData += "const uint8_t " + name_repl + "[] = {";
+		bundlerData += x.second + ",";
+
 		for (size_t i = 0; i < output.shadersize; ++i)
 		{
-			bundlerData += std::to_string((uint32_t)output.shaderdata[i]) + ",";
+			bundlerData += x.second + ",";
 		}
 		bundlerData += "};\n";
 	}
@@ -80,6 +106,7 @@ int main()
 	bundlerData += "}\n"; // namespace end
 	wi::helper::FileWrite("wiSandboxLua.h", (uint8_t*)bundlerData.c_str(), bundlerData.length());
 	std::cout << "[Sandbox Compiler] ScriptDump written to wiSandboxLua.h in " << std::setprecision(4) << timer.elapsed_seconds() << " seconds" << std::endl;
+    */
 
 	return 0;
 }
