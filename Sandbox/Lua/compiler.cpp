d3992e94a8c14b97632409ac831ab76c8f2ae8a8
Aviril <89867985+Aviril@users.noreply.github.com>
2022-01-04 01:41:56 +0300

    Update compiler.cpp

--- Sandbox/Lua/compiler.cpp
+++ Sandbox/Lua/compiler.cpp
@@ -6,7 +6,8 @@
 #include <string>
 
 std::mutex locker;
-std::string modulePath = "Lua";
+std::string moduleName = "Lua";
+std::string modulePath = moduleName;
 struct moduleTypes
 {
 	std::string moduleName;
@@ -29,32 +30,41 @@ wi::vector<moduleTypes> SandboxModule = {
     }
 };
 
-wi::unordered_map<std::string, std::string> moduleResults;
-
 int main()
 {
-	std::cout << "[Sandbox Compiler] Compiling Lua module" << std::endl;
+	std::cout << "[Sandbox Compiler] Compiling " + moduleName + " module" << std::endl;
 
 	wi::jobsystem::Initialize();
 	wi::jobsystem::context ctx;
-	modulePath += "\\";
+	modulePath += "/";
 	wi::helper::MakePathAbsolute(modulePath);
 	wi::Timer timer;
 
+    std::string bundlerData = R"(
+    #include "Core/Helpers/wiVector.h"
+    namespace SandboxLua {    
+        struct moduleTypes
+        {
+            std::string moduleName;
+            wi::vector<std::string> moduleScripts;
+        };
+        wi::vector<moduleTypes> modules = {};
+    )";
+
 	for (int i = 0; i < SandboxModule.size(); ++i)
     {
-        std::cout << SandboxModule[i].moduleName << std::endl;
+        bundlerData = bundlerData + "modules.push({" + "\"" + SandboxModule[i].moduleName + "\"" + ", {";
 		for (int j = 0; j < SandboxModule[i].moduleScripts.size(); ++j)
 		{
-            wi::vector<uint8_t> fileData;
             std::string scriptPath = SandboxModule[i].moduleScripts[j];
+            wi::vector<uint8_t> fileData;
             if (wi::helper::FileRead(modulePath + SandboxModule[i].moduleName + "/" + scriptPath, fileData))
             {
-                std::string scriptData = std::string(fileData.begin(), fileData.end());
-                wi::jobsystem::Execute(ctx, [=](wi::jobsystem::JobArgs args) {
+                wi::jobsystem::Execute(ctx, [&bundlerData, scriptPath, fileData](wi::jobsystem::JobArgs args) {
                     locker.lock();
+                    std::string scriptData = std::string(fileData.begin(), fileData.end());
+                    bundlerData += "R\"(" + scriptData + ")\",";
                     std::cout << "Script Compiled: " << scriptPath << std::endl;
-                    //moduleResults[scriptPath] = scriptData;
                     locker.unlock();
                 });
             }
@@ -65,49 +75,16 @@ int main()
                 locker.unlock();
                 std::exit(1);
             }
+            wi::jobsystem::Wait(ctx);
 		}
+        wi::jobsystem::Wait(ctx);
+        bundlerData += "}});";
     }
 	wi::jobsystem::Wait(ctx);
-	std::cout << "[Sandbox Compiler] Finished compiling Lua module in " << std::setprecision(4) << timer.elapsed_seconds() << " seconds" << std::endl;
-	timer.record();
+    bundlerData += "\n};\n";
 
-    /*
-    std::string bundlerData;
-    bundlerData += "namespace SandboxLua {\n";
-	bundlerData += "wi::unordered_map<std::string, std::string> modules {\n";
-
-    bundlerData += "wi::unordered_map<std::string, std::string> " + "Client" + "[] = {}";
-	for (auto& x : moduleResults)
-	{
-		auto& name = x.first;
-		auto& output = x.second;
-
-		std::string name_repl = name;
-		bundlerData += x.second + ",";
-
-		for (size_t i = 0; i < output.shadersize; ++i)
-		{
-			bundlerData += x.second + ",";
-		}
-		bundlerData += "};\n";
-	}
-	bundlerData += "struct ShaderDumpEntry{const uint8_t* data; size_t size;};\n";
-	bundlerData += "const wi::unordered_map<std::string, ShaderDumpEntry> shaderdump = {\n";
-	for (auto& x : moduleResults)
-	{
-		auto& name = x.first;
-		auto& output = x.second;
-
-		std::string name_repl = name;
-		std::replace(name_repl.begin(), name_repl.end(), '/', '_');
-		std::replace(name_repl.begin(), name_repl.end(), '.', '_');
-		bundlerData += "std::pair<std::string, ShaderDumpEntry>(\"" + name + "\", {" + name_repl + ",sizeof(" + name_repl + ")}),\n";
-	}
-	bundlerData += "};\n"; // map end
-	bundlerData += "}\n"; // namespace end
-	wi::helper::FileWrite("wiSandboxLua.h", (uint8_t*)bundlerData.c_str(), bundlerData.length());
-	std::cout << "[Sandbox Compiler] ScriptDump written to wiSandboxLua.h in " << std::setprecision(4) << timer.elapsed_seconds() << " seconds" << std::endl;
-    */
-
+    timer.record();
+    wi::helper::FileWrite("wiSandbox" + moduleName + ".h", (uint8_t*)bundlerData.c_str(), bundlerData.length());
+	std::cout << "[Sandbox Compiler] Finished compiling " + moduleName + " module in " << std::setprecision(4) << timer.elapsed_seconds() << " seconds" << std::endl;
 	return 0;
 }
