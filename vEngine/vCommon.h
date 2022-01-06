1cb5447e70ae46884c733d04ff737ed9fbc3081e
Aviril <89867985+Aviril@users.noreply.github.com>
2022-01-06 18:09:20 +0300

    Update vCommon.h

--- vEngine/vCommon.h
+++ vEngine/vCommon.h
@@ -1,5 +1,5 @@
-#ifndef vEngine_vCommon_H
-#define vEngine_vCommon_H
+#ifndef vEngine_vCommon
+#define vEngine_vCommon
 
 // This is a helper include file pasted into all engine headers, try to keep it minimal!
 // Do not include engine features in this file!
@@ -65,4 +65,4 @@ constexpr bool has_flag(E lhs, E rhs)
 	return (lhs & rhs) == rhs;
 }
 
-#endif //vEngine_vCommon_H
+#endif //vEngine_vCommon
