57a65903495e07e4ccb168a681e29354d92193ee
Aviril <89867985+Aviril@users.noreply.github.com>
2022-01-18 07:31:27 +0300

    Syntax fix

--- vEngine/Core/Scripting/vLua.h
+++ vEngine/Core/Scripting/vLua.h
@@ -41,7 +41,7 @@ namespace wi::lua
 	//register function to use in scripts
 	bool RegisterFunc(lua_State* L, const std::string& name, lua_CFunction function);
 	//register class
-	void RegisterLibrary(lua_State* L, const std::string& tableName, const luaL_Reg* functions, const char* namespac = NULL, const wi::vector<std::string> namespaceIndex = NULL);
+	void RegisterLibrary(lua_State* L, const std::string& tableName, const luaL_Reg* functions, const char* namespac = NULL, const wi::vector<std::string>& namespaceIndex = {});
 	//register object
 	bool RegisterObject(lua_State* L, const std::string& tableName, void* object);
 	//add function to the previously registered object
