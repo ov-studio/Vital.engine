2bb7c773f39e48322c8b00601e9f26c8ac14c072
Aviril <89867985+Aviril@users.noreply.github.com>
2022-01-18 05:17:56 +0300

    Init matrix library

--- vEngine/Core/Scripting/Lua/Math/Matrix/vMatrix.h
+++ vEngine/Core/Scripting/Lua/Math/Matrix/vMatrix.h
@@ -7,6 +7,10 @@ namespace wi::lua
     class Matrix : public XMFLOAT4X4
     {
         public:
+            // Library Binder
+            static const char libraryName[];
+            static const luaL_Reg libraryFunctions[];
+
             // Class Binder
             static const char className[];
             static Luna<Matrix>::FunctionType methods[];
@@ -17,22 +21,22 @@ namespace wi::lua
             Matrix(lua_State* L);
             static void Bind(lua_State* L);
 
-            // Static Class Methods
-            int translation(lua_State* L);
-            int rotation(lua_State* L);
-            int rotationX(lua_State* L);
-            int rotationY(lua_State* L);
-            int rotationZ(lua_State* L);
-            int quatRotation(lua_State* L);
-            int scaling(lua_State* L);
-        
+            // Library Methods
+            static int translation(lua_State* L);
+            static int rotation(lua_State* L);
+            static int rotationX(lua_State* L);
+            static int rotationY(lua_State* L);
+            static int rotationZ(lua_State* L);
+            static int quatRotation(lua_State* L);
+            static int scaling(lua_State* L);
+            static int lookTo(lua_State* L);
+            static int lookAt(lua_State* L);
+
             // Class Methods
             int getRow(lua_State* L);
             int multiply(lua_State* L);
             int add(lua_State* L);
             int transpose(lua_State* L);
             int inverse(lua_State* L);
-            int lookTo(lua_State* L);
-            int lookAt(lua_State* L);
     };
 }
