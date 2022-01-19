a47075ddf76853fa03a6cc790fca70d64645ed36
Aviril <89867985+Aviril@users.noreply.github.com>
2022-01-19 10:48:28 +0300

    Fixed indent

--- vEngine/Core/Scripting/Lua/Config/vConfig.cpp
+++ vEngine/Core/Scripting/Lua/Config/vConfig.cpp
@@ -16,86 +16,86 @@ namespace wi::lua
     };
 
     // Instance Binder
-	void Config::Bind(lua_State* L)
-	{
-		static bool initialized = false;
-		if (!initialized)
-		{
-			initialized = true;
+    void Config::Bind(lua_State* L)
+    {
+        static bool initialized = false;
+        if (!initialized)
+        {
+            initialized = true;
             RegisterLibrary(L, Config::libraryName, Config::libraryFunctions, "vEngine");
-		}
-	}
+        }
+    }
 
     // Library Methods
-	int Config::setGameSpeed(lua_State* L)
-	{
-		int argc = wi::lua::SGetArgCount(L);
-		if (argc >= 1)
-		{
-			wi::renderer::SetGameSpeed(wi::lua::SGetFloat(L, 1));
+    int Config::setGameSpeed(lua_State* L)
+    {
+        int argc = wi::lua::SGetArgCount(L);
+        if (argc >= 1)
+        {
+            wi::renderer::SetGameSpeed(wi::lua::SGetFloat(L, 1));
             wi::lua::SSetBool(L, true);
             return 1;
-		}
+        }
         wi::lua::SError(L, "Syntax: vEngine.config.setGameSpeed(float speed)");
         wi::lua::SSetBool(L, false);
-		return 0;
-	}
-	int Config::getGameSpeed(lua_State* L)
-	{
-		wi::lua::SSetFloat(L, wi::renderer::GetGameSpeed());
+        return 0;
+    }
+    int Config::getGameSpeed(lua_State* L)
+    {
+        wi::lua::SSetFloat(L, wi::renderer::GetGameSpeed());
         wi::lua::SSetBool(L, true);
-		return 1;
-	}
-	int Config::setShadowProps2D(lua_State* L)
-	{
-		int argc = wi::lua::SGetArgCount(L);
-		if (argc >= 2)
-		{
-			wi::renderer::SetShadowProps2D(wi::lua::SGetInt(L, 1), wi::lua::SGetInt(L, 2));
+        return 1;
+    }
+    int Config::setShadowProps2D(lua_State* L)
+    {
+        int argc = wi::lua::SGetArgCount(L);
+        if (argc >= 2)
+        {
+            wi::renderer::SetShadowProps2D(wi::lua::SGetInt(L, 1), wi::lua::SGetInt(L, 2));
             wi::lua::SSetBool(L, true);
             return 1;
-		}
+        }
         wi::lua::SError(L, "Syntax: vEngine.config.setShadowProps2D(int resolution, int count)");
         wi::lua::SSetBool(L, false);
-		return 0;
-	}
-	int Config::setShadowPropsCube(lua_State* L)
-	{
-		int argc = wi::lua::SGetArgCount(L);
-		if (argc >= 2)
-		{
-			wi::renderer::SetShadowPropsCube(wi::lua::SGetInt(L, 1), wi::lua::SGetInt(L, 2));
+        return 0;
+    }
+    int Config::setShadowPropsCube(lua_State* L)
+    {
+        int argc = wi::lua::SGetArgCount(L);
+        if (argc >= 2)
+        {
+            wi::renderer::SetShadowPropsCube(wi::lua::SGetInt(L, 1), wi::lua::SGetInt(L, 2));
             wi::lua::SSetBool(L, true);
             return 1;
-		}
+        }
         wi::lua::SError(L, "Syntax: vEngine.config.setShadowPropsCube(int resolution, int count)");
         wi::lua::SSetBool(L, false);
-		return 0;
-	}
-	int Config::setVSyncEnabled(lua_State* L)
-	{
-		int argc = wi::lua::SGetArgCount(L);
-		if (argc >= 1)
-		{
-			wi::eventhandler::SetVSync(wi::lua::SGetBool(L, 1));
+        return 0;
+    }
+    int Config::setVSyncEnabled(lua_State* L)
+    {
+        int argc = wi::lua::SGetArgCount(L);
+        if (argc >= 1)
+        {
+            wi::eventhandler::SetVSync(wi::lua::SGetBool(L, 1));
             wi::lua::SSetBool(L, true);
             return 1;
-		}
+        }
         wi::lua::SError(L, "Syntax: vEngine.config.setVSyncEnabled(bool state)");
         wi::lua::SSetBool(L, false);
-		return 0;
-	}
-	int Config::setOcclusionCullingEnabled(lua_State* L)
-	{
-		int argc = wi::lua::SGetArgCount(L);
-		if (argc >= 1)
+        return 0;
+    }
+    int Config::setOcclusionCullingEnabled(lua_State* L)
+    {
+        int argc = wi::lua::SGetArgCount(L);
+        if (argc >= 1)
         {
-			wi::renderer::SetOcclusionCullingEnabled(wi::lua::SGetBool(L, 1));
+            wi::renderer::SetOcclusionCullingEnabled(wi::lua::SGetBool(L, 1));
             wi::lua::SSetBool(L, true);
             return 1;
         }
         wi::lua::SError(L, "Syntax: vEngine.config.setOcclusionCullingEnabled(bool state)");
         wi::lua::SSetBool(L, false);
-		return 0;
-	}
+        return 0;
+    }
 };
