b2391764e156dc6d84676fde8c6a48fd1f1b4773
Aviril <89867985+Aviril@users.noreply.github.com>
2022-01-29 10:10:20 +0300

    Added `table.clone`

---
+++ Sandbox/Lua/Shared/table.lua
@@ -0,0 +1,29 @@
+----------------------------------------------------------------
+--[[ Resource: vEngine: Lua Module
+     Script: Shared: table.lua
+     Author: vStudio
+     Developer(s): Aviril, Mario, Tron
+     DOC: 02/01/2022
+     Desc: Table Module ]]--
+----------------------------------------------------------------
+
+
+----------------
+--[[ Module ]]--
+----------------
+
+--Function: Clones a table
+vEngine.table.lerp = function(baseTable, isRecursive)
+    local API = "vEngine.table.clone"
+    assert(not baseTable or (type(baseTable) ~= "table"), vEngine.__postAPIMessage(API, "Invalid baseTable", 3))
+
+    local clonedTable = {}
+    for i, j in pairs(baseTable) do
+        if (type(j) == "table") and isRecursive then
+            clonedTable[i] = vEngine.table.clone(j, true)
+        else
+            clonedTable[i] = j
+        end
+    end
+    return clonedTable
+end
